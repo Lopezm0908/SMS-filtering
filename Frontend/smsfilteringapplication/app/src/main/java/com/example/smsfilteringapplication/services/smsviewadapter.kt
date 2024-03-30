47236b8444bf704499a42d95e4bce1a1b9151013
Jeremiah Warinner <warinnerjeremiah@gmail.com>
2024-03-29 22:42:46 -0500

    blacklist main view is finished and ready for formatting, individual items on list view are located in separate xml file

---
+++ Frontend/smsfilteringapplication/app/src/main/java/com/example/smsfilteringapplication/services/smsviewadapter.kt
@@ -0,0 +1,47 @@
+package com.example.smsfilteringapplication.services
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import com.example.smsfilteringapplication.R
+
+
+class smsviewadapter(context: Context, fromarray: ArrayList<String>, bodyarray: ArrayList<String>): BaseAdapter() {
+        private val mContext: Context
+
+        val from_array = fromarray
+        val body_array = bodyarray
+        init{
+            mContext = context
+        }
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+        // responsible for how many rows
+        override fun getCount(): Int {
+            return body_array.size
+        }
+        override fun getItem(position: Int): Any {
+            return "Test String"
+        }
+
+        //responsible for rendering each row
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val layoutInflater = LayoutInflater.from(mContext)
+            val blacklistRow = layoutInflater.inflate(R.layout.message_selectable_item, parent, false)
+
+
+            val fromTextView = blacklistRow.findViewById<TextView>(R.id.from_data)
+            val bodyTextView = blacklistRow.findViewById<TextView>(R.id.body_data)
+
+
+            fromTextView.text = from_array.get(position)
+            bodyTextView.text = body_array.get(position)
+
+            return blacklistRow
+        }
+
+    }
