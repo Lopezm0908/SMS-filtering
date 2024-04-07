d852f04792f81d670b9cec7f5b2522dedffefa7d
CameronJ2 <jonescameron980p@gmail.com>
2024-04-06 20:56:15 -0500

    Got first iteration of BERT finished. Seems very innacurate, so still needs fine-tuning.

---
+++ Backend/LRTest.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+
+public class LRTest {
+    public static void main(String[] args) throws IOException {
+        TestMessage testing = new TestMessage();
+
+        String message = "XXXMobileMovieClub: To use your credit, click the WAP link in the next txt message or click here>> http://wap. xxxmobilemovieclub.com?n=QJKGIGHJJGCBL";
+        testing.Test(message);
+    }
+}
