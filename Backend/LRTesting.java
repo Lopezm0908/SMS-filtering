6ebee0d7d40794b465d5eff3cfca6c726f203579
CameronJ2 <jonescameron980p@gmail.com>
2024-04-06 21:03:21 -0500

    Added Logistic Regression testing class

---
+++ Backend/LRTesting.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class LRTesting {
+    
+    public static void main (String[] args) throws IOException {
+        TestMessage testing = new TestMessage();
+
+        String message = "";
+        testing.Test(message);
+    }
+}
