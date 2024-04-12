45b791fd29ea4b55a6f01976e2361007b1f04f1e
CameronJ2 <jonescameron980p@gmail.com>
2024-04-11 23:18:40 -0500

    new query function

--- Frontend/smsfilteringapplication/app/src/main/java/com/example/smsfilteringapplication/dataclasses/DatabaseDriver.kt
+++ Frontend/smsfilteringapplication/app/src/main/java/com/example/smsfilteringapplication/dataclasses/DatabaseDriver.kt
@@ -21,40 +21,20 @@ enum class QueryField(val value: String) {
     ID("id")
 }
 fun stringItemQueryToArrayList(type : String, data : QueryField) : ArrayList<String> {
-//fun stringItemQueryToArrayList(type : String, data : String = "") : ArrayList<String> {
     val arrayListOfItems = arrayListOf<String>()
     arrayListOfItems.clear()
     val initialQuery = realm.query<StringItem>("type = $0", type).find().toList()
-
     for (i in initialQuery) {
-        val field = StringItem::class.java.getDeclaredField(data.value)
-        field.isAccessible = true
-        val value = field.get(i)
-        arrayListOfItems.add(value as String)
+        if (data == QueryField.CONTENT){
+            arrayListOfItems.add(i.content)
+        }
+        if (data == QueryField.SENDER){
+            arrayListOfItems.add(i.sender)
+        }
     }
 
     return arrayListOfItems
 }
-//    if (type == "Whitelist" || type == "KeyWord") {
-//        for (i in initialQuery) {
-//            arrayListOfItems.add(i.content)
-//        }
-//    } else if (type == "Eval") {
-//        for (i in initialQuery) {
-//            arrayListOfItems.add(i.content)
-//        }
-//    }
-//    if (data == "content") {
-//        for (i in initialQuery) {
-//            arrayListOfItems.add(i.content)
-//        }
-//    } else if (data == "sender") {
-//        for (i in initialQuery) {
-//            arrayListOfItems.add(i.sender)
-//        }
-//    }
-//    return arrayListOfItems
-//}
 
 suspend fun removeItem (newNumber : String, type : String){
     realm.write{
@@ -65,33 +45,35 @@ suspend fun removeItem (newNumber : String, type : String){
         }
     }
 }
+
+
 suspend fun addItem (newNumber : String, type : String){
     realm.write{
         val testNum = StringItem().apply{
             content = newNumber
             this@apply.type = type
+
         }
-        copyToRealm(testNum, updatePolicy = UpdatePolicy.ALL)
-//        when (type) {
-//            "Whitelist", "KeyWord" -> {
-//                val testNum = StringItem().apply{
-//                    content = newNumber
-//                    this@apply.type = type
-//                }
-//                copyToRealm(testNum, updatePolicy = UpdatePolicy.ALL)
-//            }
-//            "Eval" -> {
-//                val testNum = StringItem().apply{
-//                    this@apply.sender = sender
-//                    content = newNumber
-//                    this@apply.type = type
-//                }
-//                copyToRealm(testNum, updatePolicy = UpdatePolicy.ALL)
-//            }
-//            else -> {
-//                println("Invalid type.")
-//            }
-//        }
+        when (type) {
+            "Whitelist", "KeyWord" -> {
+                val testNum = StringItem().apply{
+                    content = newNumber
+                    this@apply.type = type
+                }
+                copyToRealm(testNum, updatePolicy = UpdatePolicy.ALL)
+            }
+            "Eval" -> {
+                val testNum = StringItem().apply{
+                    this@apply.sender = sender
+                    content = newNumber
+                    this@apply.type = type
+                }
+                copyToRealm(testNum, updatePolicy = UpdatePolicy.ALL)
+            }
+            else -> {
+                println("Invalid type.")
+            }
+        }
     }
 }
 
